5c3790f3cbf170dd63da930aa9e48a7487db5a9e
Mymister <804563996@qq.com>
2017-11-20 21:13:45 +0800

    添加返回按钮

--- src/component/common/header.vue
+++ src/component/common/header.vue
@@ -1,20 +1,23 @@
 <template>
 <header>
-    <mt-header fixed title="江买卖"></mt-header>
+    <mt-header fixed title="江买卖">
+        <!-- 返回按钮只在非首页出现 -->
+            <!-- 通过$route.path拿到当前路径比较 -->
+            <!-- 通过$router.go(-1)方法实现页面返回功能 -->
+        <mt-button v-if="$route.path != '/index'" v-on:click="$router.go(-1)" icon="back" slot="left"></mt-button>
+    </mt-header>
     <i></i>
 </header>
     
 </template>
 
 <script>
-export default {
-  
-}
+export default {};
 </script>
 
 <style scoped>
-header i{
-    display: block;
-    height: 40px;
+header i {
+  display: block;
+  height: 40px;
 }
 </style>
