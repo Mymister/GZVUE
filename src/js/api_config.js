7d8cbf26d7681d66a441438808f3a1f6e1a70911
Mymister <804563996@qq.com>
2017-11-21 00:54:49 +0800

    完整的商品列表与详情的小部分

--- src/js/api_config.js
+++ src/js/api_config.js
@@ -15,7 +15,16 @@ export default{
     getPT: `${domain}/getthumimages/`,  // 该接口后面需要一个图片id: /getthumimages/:id
 
     
-    // 商品接口
+    // 商品相关接口
+    goodsL: `${domain}/getgoods/`,          // 该接口后面需要一个页码: /getgoods/?pageindex=number
+    goodsD: `${domain}/goods/getdesc/`,     // 该接口后面需要一个商品id: /getdesc/:id
+    goodsT: `${domain}/getthumimages/`,     // 该接口后面需要一个商品id: /getthumimages/:id
+    goodsP: `${domain}/getinfo/`,           // 该接口后面需要一个商品id: /getinfo/:id
 
-    // 评论接口
+    // 购物车相关接口
+    shopcL: `${domain}/goods/getshopcarlist/`,  // 该接口后面需要一串id: /getshopcarlist/:ids
+
+    // 评论相关接口
+    commentL: `${domain}/getcomments/`,     // 该接口后面需要一个id: /getcomments/:id
+    commentS: `${domain}/postcomment/`,     // 该接口后面需要一个id: /postcomment/:id, 该需要content内容
 }
