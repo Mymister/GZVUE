caa9cc74a672b54dbbb75947510a4418c7068326
Mymister <804563996@qq.com>
2017-11-20 16:32:02 +0800

    集成mint-ui组件库-header

--- src/js/main.js
+++ src/js/main.js
@@ -1,15 +1,20 @@
-// 导入第三方包
+//导入第三方包
 import Vue from 'vue';
+import MintUi from 'mint-ui';
+import 'mint-ui/lib/style.css';
+import Common from '../component/common';// 自动找到index.js引入
+
+//启用vue插件
+Vue.use(MintUi);
+Vue.use(Common);
 
 //导入根组件
-import AppComponent from '../component/App.vue';
+import AppComponent from '../component/App.vue'
 
-
-//渲染根组件，启动项目
+// 渲染根组件, 启动项目
 new Vue({
     el:'#app',
     render(createNode){
         return createNode(AppComponent);
     }
 })
-
