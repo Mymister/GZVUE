3a28e5bfa56958d7cf8c3c7cb4b85c5427aa04f4
Mymister <804563996@qq.com>
2017-11-20 17:06:04 +0800

    集成axios&实现首页

--- src/component/common/index.js
+++ src/component/common/index.js
@@ -1,12 +1,13 @@
-// 编写属于自己的公共Vue组件库
+//编写属于自己的公共VUE组件库
 import HeaderComponent from './header.vue';
 import FooterComponent from './footer.vue';
 
-// Vue插件要求提供一个install方法, 这个方法会被注入Vue
-// 需要我们调用Vue的filter component directive去扩展功能
-export default {
-    install(Vue) {
-        Vue.component('app-header', HeaderComponent);
-        Vue.component('app-footer', FooterComponent);
+
+//Vue插件要求提供install方法，这个方法会被注入Vue
+//需要我们调用Vue的filter component directive
+export default{
+    install(Vue){
+        Vue.component('app-header',HeaderComponent);
+        Vue.component('app-footer',FooterComponent);
     }
-};
+}
--- src/js/main.js
+++ src/js/main.js
@@ -2,18 +2,26 @@
 import Vue from 'vue';
 import MintUi from 'mint-ui';
 import 'mint-ui/lib/style.css';
-import Common from '../component/common';// 自动找到index.js引入
+import Common from '../component/common';//自动引入index.js
 import 'mui/dist/css/mui.css';
 import 'mui/examples/hello-mui/css/icons-extra.css';
+import axios from 'axios';
 
-//启用vue插件
+
+
+//启用VUE插件
 Vue.use(MintUi);
 Vue.use(Common);
+Vue.prototype.axios = axios; // 把axios库放置到原型,其他组件直接可以拿到axios对象
+
 
 //导入根组件
-import AppComponent from '../component/App.vue'
+import AppComponent from '../component/App.vue';
 
-// 渲染根组件, 启动项目
+
+
+
+//渲染组件，启动项目
 new Vue({
     el:'#app',
     render(createNode){
